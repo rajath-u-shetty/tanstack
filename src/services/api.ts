1899148829888a9a94758a9d7107cd3735bb6530
Rajath U Shetty <rajathushetty56@gmail.com>
2024-01-17 22:53:49 +0530

    added todos

---
+++ src/services/api.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { Todo } from "../types/todo";
+
+const BASE_URL = "http://localhost:8080";
+const axiosInstance = axios.create({ baseURL: BASE_URL })
+
+export const getTodosIds = async () => {
+    return (await axiosInstance.get<Todo[]>("todos")).data.map((todo) => todo.id);
+}
+
+export const getTodo =async (id: number) => {
+   return (await axiosInstance.get<Todo>(`todos/${id}`)).data;
+}
---
+++ src/types/todo.ts
@@ -0,0 +1,7 @@
+
+export interface Todo {
+    checked: boolean,
+    title: string,
+    desciption: string,
+    id: number;
+}
